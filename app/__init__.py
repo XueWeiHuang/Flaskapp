10f8412a62d4519872441e80420c770f4786376a
Joe Huang <Joe.Huang@validusholdings.com>
2019-08-02 14:59:27 +0000

    finish until error by emails

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
-
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +14,9 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth=None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+
+from app import routes, models, errors
